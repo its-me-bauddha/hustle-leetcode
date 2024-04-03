47b27c49fd19d9942ce69be40e3f91be86791408
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-04-03 09:04:08 +0530

    Time: 2 ms (38.89%) | Memory: 7.5 MB (51.91%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    int countDigit(int n){
-        int count =0;
-        while(n >0){
-            count++;
-            n /= 10;
-        }
-        return count;
-    }
-    int reverse(int x) {
+ int reverse(int x) {
         long long reverseDigit = 0;
         
         int N = abs(x) ;
-        int count = countDigit(N);
-
-      
-       while(N!=0)
-       {
+        while(N!=0)
+        {
         int digit = N%10;
         reverseDigit = reverseDigit*10 + digit;
         N = N/10;
-       }
+        }
         if(x >= 0 &&reverseDigit <= INT_MAX )
         return reverseDigit;
         else if(reverseDigit > INT_MAX) return 0;
