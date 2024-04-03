cd188e8bca50fc06f4fbba9d597002efcd6197b9
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-04-03 09:09:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (48.08%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int reverse(int x){
+        long long reverseDigit = 0;
+        
+        int N = abs(x) ;
+        while(N!=0)
+        {
+        int digit = N%10;
+        reverseDigit = reverseDigit*10 + digit;
+        N = N/10;
+        }
+        if(x >= 0 &&reverseDigit <= INT_MAX )
+        return reverseDigit;
+        else if(reverseDigit > INT_MAX) return 0;
+        else 
+            return  reverseDigit* -1;
+    }
+    bool isPalindrome(int x) {
+        int reverseX = reverse(x);
+        if(x < 0 ) return false;
+        if(reverseX == x) return true;
+        else return false;
+    }
+};
