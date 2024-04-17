53e80d8684559fc700eff5b033197618326f3634
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-04-17 21:20:45 +0530

    Time: 5 ms (52.91%) | Memory: 8.1 MB (82.47%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    string addStringsRE(string &num1,int p1, string &num2,int p2,int carry =0 ) {
+    void addStringsRE(string &num1,int p1, string &num2,int p2,string& ans,int carry =0 ) {
             // base case
             if(p1 < 0 && p2 < 0 ){
                 if(carry != 0 ){
-                    return string(1,carry+'0');
+                     ans.push_back(carry + '0');
                 }
-                return "";
+                return;
             }
 
             // ek case solve kro
@@ -17,14 +17,15 @@ public:
             int csum = n1+n2 + carry;
             int digit  = csum%10;
             carry = csum/10;
-            string ans = "";
+            
             ans.push_back(digit + '0');
 
-            ans += addStringsRE(num1,p1-1,num2,p2-1,carry);
-            return ans;
+            addStringsRE(num1,p1-1,num2,p2-1,ans,carry);
+            
     }
     string addStrings(string num1, string num2) {
-        string ans = addStringsRE(num1,num1.size()-1,num2,num2.size()-1);
+        string ans = "";
+        addStringsRE(num1,num1.size()-1,num2,num2.size()-1,ans);
         reverse(ans.begin(),ans.end());
         return ans;
     }
