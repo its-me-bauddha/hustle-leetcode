5f25f613c841615766c498cdbf4b79a973542a23
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-04-30 09:56:39 +0530

    Time: 11 ms (12.12%) | Memory: 13.6 MB (10.54%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int result =0 ;
+        // int result =0 ;
 
-        for(int k = 0;k<=31;k++){
-            int temp = (1<<k);  // left shift k times
-            int countOnes = 0 ;
+        // for(int k = 0;k<=31;k++){
+        //     int temp = (1<<k);  // left shift k times
+        //     int countOnes = 0 ;
          
-            for(int &num : nums){
-                if((num & temp) == 0 ){// the kth bit of num is zero
+        //     for(int &num : nums){
+        //         if((num & temp) == 0 ){// the kth bit of num is zero
                    
-                }else{
-                    countOnes++;
-                }
+        //         }else{
+        //             countOnes++;
+        //         }
 
-            }
+        //     }
 
-            if(countOnes%3  == 1){
-                result = (result | temp); // set kth bit
-            }
+        //     if(countOnes%3  == 1){
+        //         result = (result | temp); // set kth bit
+        //     }
 
+        // }
+        // return result;
+
+       unordered_map<int,int>mp;
+        for(auto num:nums){
+            mp[num]++;
         }
-        return result;
+
+        for(auto it :mp){
+            if(it.second % 3 == 1) return it.first;
+        }
+
+        return -1;
     }
 };
