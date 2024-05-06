7acb2a2efdced923fbdb1cc762176c190c7a76ce
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-05-06 21:35:53 +0530

    Time: 321 ms (9.67%) | Memory: 166 MB (52.57%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -24,35 +24,75 @@ public:
         }
         return prev;
     } 
+    ListNode* recSolve(ListNode* head){
+        if(head ==NULL || head -> next == NULL){
+            return head;
+        }
+
+        ListNode * nextNode = recSolve(head->next);
+
+        if(head -> val < nextNode ->val){
+            delete head;
+            return nextNode;
+        } 
+
+            head ->next = nextNode;
+            return head;
+      
+
+    }
     ListNode* removeNodes(ListNode* head) {
-            // ListNode* reveseList = reverseNode(head);
+            
 
-            stack<ListNode*> st;
-            ListNode*temp = head;
-            while(temp != NULL){
-                st.push(temp);
-                temp = temp->next;
-            }
-            temp = st.top();
-            st.pop();
-            int maxNode = temp->val;
-            ListNode* ans = new ListNode(maxNode);
+            // stack<ListNode*> st;
+            // ListNode*temp = head;
+            // while(temp != NULL){
+            //     st.push(temp);
+            //     temp = temp->next;
+            // }
+            // temp = st.top();
+            // st.pop();
+            // // int maxNode = temp->val;
+            // ListNode* ans = new ListNode(maxNode);
 
-            while(!st.empty()){
-                   temp = st.top();
-                    st.pop();
-                    if(temp->val < maxNode){
-                       continue;
-                    }else{
+            // while(!st.empty()){
+            //        temp = st.top();
+            //         st.pop();
+            //         if(temp->val < maxNode){
+            //            continue;
+            //         }else{
                          
-                       ListNode* newNode = new ListNode(temp->val);
-                        newNode->next = ans;
-                        ans = newNode;
-                        maxNode = temp->val;
-                    }
-            }
+            //            ListNode* newNode = new ListNode(temp->val);
+            //             newNode->next = ans;
+            //             ans = newNode;
+            //             maxNode = temp->val;
+            //         }
+            // }
 
-            return ans;
+            // // return recSolve(head);
+
+
+         head = reverseNode(head);
+
+        ListNode * prev = NULL;
+        ListNode* curr = head;
+        int maxNode = -1;
+        
+         while(curr != NULL){
+            maxNode = max(maxNode , curr->val);
+            if(maxNode > curr->val){
+                prev -> next = curr->next;
+                ListNode * temp = curr;
+                curr = curr->next;
+                delete temp;
+
+            }else{
+                prev = curr;
+                curr = curr->next;
+            }
+         }
+
+         return reverseNode(head);
     }
 
 };
