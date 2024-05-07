2959a230ca3b853d3a14a58ad739fd9e4551c4a4
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-05-07 21:13:24 +0530

    Time: 172 ms (66.48%) | Memory: 120.7 MB (87.76%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
@@ -25,28 +25,48 @@ public:
         return prev;
     }
     ListNode* doubleIt(ListNode* head) {
-       head = reverse(head);
+    //    head = reverse(head);
        
-       ListNode* curr = head;
-    ListNode * prev = NULL;
-        int carry = 0 ;
-       while(curr != NULL){
-                int currVal = curr->val;
-                int doubleVal = carry + (currVal*2);
-                int digit = doubleVal  % 10;
-                carry = doubleVal /10;
+    //    ListNode* curr = head;
+    // ListNode * prev = NULL;
+    //     int carry = 0 ;
+    //    while(curr != NULL){
+    //             int currVal = curr->val;
+    //             int doubleVal = carry + (currVal*2);
+    //             int digit = doubleVal  % 10;
+    //             carry = doubleVal /10;
 
-                curr->val = digit ;
-                prev = curr;
-                curr = curr->next;
-       }
+    //             curr->val = digit ;
+    //             prev = curr;
+    //             curr = curr->next;
+    //    }
         
-       if(carry > 0 ){
-            ListNode * newNode = new ListNode(carry);
-            prev -> next = newNode;
+    //    if(carry > 0 ){
+    //         ListNode * newNode = new ListNode(carry);
+    //         prev -> next = newNode;
         
-       }
-      return  reverse(head);
+    //    }
+    //   return  reverse(head);
+
+    if(head -> val >= 5 ){
+        ListNode * newHead = new ListNode(0);
+        newHead ->next =head;
+        head = newHead;
+
+        // head = new ListNode(0,head)
+    }
+
+
+    ListNode* curr = head;
+    while(curr != NULL){
+        curr -> val = (curr->val * 2) %10;
+
+        if(curr->next != NULL &&  curr->next->val >= 5 ){
+            curr->val += 1;
+        }
+        curr = curr->next;
+    }
+    return head;
       
     }
 };
