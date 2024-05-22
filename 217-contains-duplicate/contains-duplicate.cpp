48209dcb5d840528aef7206a22ef920698bab7b8
Harsh  Bauddha <105487622+its-me-bauddha@users.noreply.github.com>
2024-05-22 15:37:21 +0530

    Time: 97 ms (44.40%) | Memory: 73.1 MB (44.10%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int > mp;
-        for(auto it :nums) mp[it]++;
-
-        for(auto i :mp){
-            if(i.second >= 2) return true;
+    bool containsDuplicate (vector<int>& nums) {
+        unordered_set<int>hset;
+        for(auto num :nums){
+            if(hset.count(num) > 0){
+                return true;
+            }
+            hset.insert(num);
         }
         return false;
     }
-
 };
